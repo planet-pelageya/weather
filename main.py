cfbb9363044594a39d969f6ffaa3be4968139bda
planet-pelageya <ryazancev_0101@mail.ru>
2024-06-05 02:17:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import requests 
-payload ={"lang":"ru","n":'',"T":'',"q":'',"M":''} 
-result = requests.get("https://wttr.in/Череповец",params = payload) 
+import requests
 
-print(result.text)
+cities = ["Лондон","Череповец", "Шереметьево"]
+payload ={"lang":"ru","n":'',"T":'',"q":'',"M":''}
+
+def temperature(*args):
+    for i in cities:
+        r = requests.get(f"https://wttr.in/{i}", params=payload)
+        r.raise_for_status()
+        print(r.text)
+
+if __name__ == '__main__':
+    temperature(cities)
