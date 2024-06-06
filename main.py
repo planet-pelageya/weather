505ec499d9be1407634d1e88be8dfe022ff1aa1e
planet-pelageya <ryazancev_0101@mail.ru>
2024-06-06 07:04:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-
 import requests
 
-cities = ["Лондон","Череповец", "Шереметьево"] 
+cities = ["Лондон","Череповец", "Шереметьево"]
 #я оставил переменную выше шлобальной, чтобы если что можно было поменять города
 
-def temperature():
+def get_temperature():
     payload ={"lang":"ru","n":'',"T":'',"q":'',"M":''}
     for i in cities:
         r = requests.get(f"https://wttr.in/{i}", params=payload)
         r.raise_for_status()
         print(r.text)
 
-if __name__ == '__main__':
-    temperature()
-
+get_temperature()
