a1ee47df077852eda1eae4a2f2d4521c79195496
planet-pelageya <ryazancev_0101@mail.ru>
2024-06-06 06:52:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,16 @@
+
 import requests
 
-cities = ["Лондон","Череповец", "Шереметьево"]
-payload ={"lang":"ru","n":'',"T":'',"q":'',"M":''}
+cities = ["Лондон","Череповец", "Шереметьево"] 
+#я оставил переменную выше шлобальной, чтобы если что можно было поменять города
 
-def temperature(*args):
-    for i in cities:
-        try:
-            r = requests.get(f"https://wttr.in/{i}", params=payload)
-            print(r.text)
-        except:
-            return None
-
-if __name__ == '__main__':
-    temperature(cities)
-
-# Это мой готовый вариант. При нём у меня не выскакивает куча красного текста и ошибок, ниже укажу как я понял нужно работать с raise_for_status() и status.code
-_________________________________________________________________________________
-import requests
-
-cities = ["Лондон","Череповец", "Шереметьево"]
-payload ={"lang":"ru","n":'',"T":'',"q":'',"M":''}
-
-def temperature(*args):
+def temperature():
+    payload ={"lang":"ru","n":'',"T":'',"q":'',"M":''}
     for i in cities:
         r = requests.get(f"https://wttr.in/{i}", params=payload)
-        if r.status_code == 200:
-            print(r.text)
-        else:
-            return None
+        r.raise_for_status()
+        print(r.text)
 
 if __name__ == '__main__':
     temperature(cities)
-___________________________________________________________________________
 
-import requests
-
-cities = ["Лондон","Череповец", "Шереметьево"]
-payload ={"lang":"ru","n":'',"T":'',"q":'',"M":''}
-
-def temperature(*args):
-    for i in cities:
-        try:
-            r = requests.get(f"https://wttr.in/{i}", params=payload)
-            r.raise_for_status()
-            print(r.text)
-        except:
-            None
-
-if __name__ == '__main__':
-    temperature(cities)
